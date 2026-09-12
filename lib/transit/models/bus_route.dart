3af1e58216ef14150d1aef967c84c7a9d1d8a156
fcwe1113 <fcwe1113@gmail.com>
2026-09-12 17:56:42 +0100

    implemented bus route api and json parser

--- lib/transit/models/bus_route.dart
+++ lib/transit/models/bus_route.dart
@@ -6,10 +6,10 @@ class BusRoute {
   final String id;
   final Map<String, String> names; // may hv locale diffs, maybe remove if not needed
   final String routeNumber;
-  final int bound; // change if not adapting to new apis
+  final String bound; // change if not adapting to new apis
   final BusStop origin; // may be placeholder until resolved
   final BusStop destination; // same here
-  final String destinationText; // the destination showed normally
+  final Map<String, String> destinationText; // the destination showed normally
   final String providerCode;
 
   const BusRoute({
--- lib/transit/providers/transit_provider.dart
+++ lib/transit/providers/transit_provider.dart
@@ -3,11 +3,14 @@
 
 import 'package:bus_arrival_notification_app/transit/models/bus_stop.dart';
 
+import '../models/bus_route.dart';
+
 abstract class TransitProvider {
   String get providerCode;
   String get providerName;
   String get IconAsset; // contains the link to the icon
 
   Future<List<BusStop>> fetchStops();
+  Future<List<BusRoute>> fetchRoute();
   // Future<List<StopPrediciton>> fetchPredicitons(String stopID);
 }
