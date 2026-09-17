2327233fd59c9003869e0e6a8bb7ea3ee2ac6d50
fcwe1113 <fcwe1113@gmail.com>
2026-09-17 18:23:53 +0100

    refactored project directory structure

--- lib/provider_registry.dart
+++ lib/provider_registry.dart
@@ -1,5 +1,5 @@
-import 'package:bus_arrival_notification_app/transit/providers/hk/kmb_provider.dart';
-import 'package:bus_arrival_notification_app/transit/providers/transit_provider.dart';
+import 'package:bus_arrival_notification_app/transit/locale/hk/providers/kmb_provider.dart';
+import 'package:bus_arrival_notification_app/transit/transit_provider.dart';
 import 'package:bus_arrival_notification_app/transit/services/api_caller.dart';
 import 'package:bus_arrival_notification_app/transit/services/transit_cache_service.dart';
 import 'package:bus_arrival_notification_app/transit/services/transit_update_scheduler.dart';
